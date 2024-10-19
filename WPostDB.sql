cca8e3072da4dbcc71ef8bd024a41e5504c3fc4d
Marwa-Essam81 <96136636+Marwa-Essam81@users.noreply.github.com>
2024-10-19 23:13:27 +0300

    Add files via upload

---
+++ WPostDB.sql
@@ -0,0 +1,42 @@
+-- Database export via SQLPro (https://www.sqlprostudio.com/)
+-- Exported by omarsayed at 17-10-2024 01:40.
+-- WARNING: This file may contain descructive statements such as DROPs.
+-- Please ensure that you are running the script at the proper location.
+
+
+-- BEGIN TABLE Contents
+CREATE TABLE `Contents` (
+  `pid` varchar(50) NOT NULL,
+  `did` varchar(50) NOT NULL,
+  `data` text NOT NULL,
+  `pos` int NOT NULL,
+  PRIMARY KEY (`pid`),
+  KEY `documentLink` (`did`),
+  CONSTRAINT `documentLink` FOREIGN KEY (`did`) REFERENCES `Documents` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- END TABLE Contents
+
+-- BEGIN TABLE Documents
+CREATE TABLE `Documents` (
+  `id` varchar(50) NOT NULL,
+  `title` text NOT NULL,
+  `kicker` text NOT NULL,
+  `url` text NOT NULL,
+  `author` text NOT NULL,
+  `date` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- END TABLE Documents
+
+-- BEGIN TABLE Mentions
+CREATE TABLE `Mentions` (
+  `pid` varchar(50) NOT NULL,
+  `pos` int NOT NULL,
+  `mentions` text NOT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- END TABLE Mentions
+
